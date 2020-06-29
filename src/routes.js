e226b1019a681cf5e9caddcd5b243f347e2db40e
Paulo Silva dos Reis <Paulosilvadosreis2057@gmail.com>
2020-06-29 20:57:56 -0300

    Adição da página de criação de produto e aperfeiçoamento dos layouts
    A página de criação de um produto foi implementada, melhorias na estrutura dos layouts feita

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,12 @@ const express = require('express')
 
 const routes = express.Router()
 
-routes.get("/", (req, res) => {
+const cars = require('./app/controllers/cars')
+
+routes.get("/", function (req, res) {
     return res.render("index")
 })
 
+routes.get("/products/create", cars.create)
 
 module.exports = routes
