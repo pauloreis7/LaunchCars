957f460958959098b931730a76eed7627b96856a
Paulo Silva dos Reis <Paulosilvadosreis2057@gmail.com>
2020-07-05 10:08:14 -0300

    Adição de novas features de mácara de inputs e aprofundando o asssincronismo
    Utilização mais profunda de funções assincronas  foi feita e novas máscaras de inputs inserida

--- public/scripts.js
+++ public/scripts.js
@@ -13,10 +13,32 @@ const Mask = {
 
         value = value.replace(/\D/g, "")
 
-        return value = new Intl.NumberFormat('en-IN', {
+        return value = new Intl.NumberFormat('en-US', {
             style: 'currency',
             currency: 'USD'
         }).format(value/100)
+    },
+
+    formatPercent(value) {
+
+        value = value.replace(/\D/g, "")
+
+        return new Intl.NumberFormat('en-US', {
+            style: 'percent',
+            minimumFractionDigits: 2,
+            minimumIntegerDigits: 2,
+            maximumSignificantDigits: 6
+            
+        }).format(value/10000)
+    },
+
+    formatCPF(value) {
+
+       value = value.replace(/\D/g, "")
+
+       value = value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, "\$1.\$2.\$3-\$4")
+
+       return value
     }
 }
 
@@ -47,18 +69,16 @@ const Mask = {
 
 //                     return console.log("Sou o 5")
 //                 })
-
 //                 console.log("Sou o 4")
 //             })
 //             console.log("Sou o 3")
 //         })
-
 //         console.log("Sou o 2")
 //     })
-// console.log("Sou o 1")
+//     console.log("Sou o 1")
 // })
 
-// Fazendo com Promises:
+// Fazendo com Promises / async await:
 
 function calculate(number, lastResult) {
 
@@ -67,18 +87,35 @@ function calculate(number, lastResult) {
         setTimeout(function () {
 
             mult = number * 2
-            final = mult + lastResult
+            final = Math.floor(mult + lastResult)
 
             console.log(final)
             
             resolve(final)
-            
+                        
         }, 1000, number, lastResult)
 
     })
 }
 
-calculate(1, 0).then((res) => { calculate(2, res)
-.then((res) => { calculate(3, res)
-.then((res) => { calculate(4, res)
-.then((res) => { calculate(5, res) }) }) })})
+// calculate(1, 0).then((res) => { calculate(2, res)
+// .then((res) => { calculate(3, res)
+// .then((res) => { calculate(4, res)
+// .then((res) => { calculate(5, res) }) }) })})
+
+async function results() {
+    
+    let result
+
+    result = await calculate(1, 0) // 2
+
+    result = await calculate(2, result)  // 6
+
+    result = await calculate(3, result) // 12
+
+    result = await calculate(4, result) // 20
+
+    result = await calculate(5, result) // 30
+}
+
+// results()
